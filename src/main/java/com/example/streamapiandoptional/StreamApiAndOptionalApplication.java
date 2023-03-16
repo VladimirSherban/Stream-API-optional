1f73e97ba1f7caa901731ae0303cbfc9d0694aa2
VladimirSherban <Vovan_Sh_94@mail.ru>
2023-03-17 02:46:40 +0300

    First commit!

---
+++ src/main/java/com/example/streamapiandoptional/StreamApiAndOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamapiandoptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiAndOptionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApiAndOptionalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/streamapiandoptional/StreamApiAndOptionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamapiandoptional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiAndOptionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
