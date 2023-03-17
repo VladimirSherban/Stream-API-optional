6c68b99a0f7a0d0785ddb03b067e437e071e2478
VladimirSherban <Vovan_Sh_94@mail.ru>
2023-03-17 04:07:52 +0300

    Homework2!

--- src/main/java/com/example/streamapiandoptional/Main.java
+++ src/main/java/com/example/streamapiandoptional/Main.java
@@ -1,8 +1,6 @@
 package com.example.streamapiandoptional;
 
 import java.util.*;
-import java.util.function.BiConsumer;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,25 +15,18 @@ public class Main {
 
         System.out.println(integerList);
 
-        Optional<Integer> max = integerList.stream().max(Comparator.naturalOrder());
-        Optional<Integer> min = integerList.stream().min(Comparator.naturalOrder());
-
-        System.out.println("Максимальное число - " + max + "\nМинимальное число - " + min);
-
-
+        main.findMinMax(integerList);
         main.findEvenNumbersInList(integerList); // 2 Задание
 
-
     }
 
-    private <T> void findMinMax(Stream<? extends T> stream,
-                                Comparator<? super T> order,
-                                BiConsumer<? super T, ? super T> minMaxConsumer) {
-
+    private void findMinMax(List<Integer> integerList) {
+        Optional<Integer> max = integerList.stream().max(Comparator.naturalOrder());
+        Optional<Integer> min = integerList.stream().min(Comparator.naturalOrder());
+        System.out.println("Максимальное число - " + max + "\nМинимальное число - " + min);
     }
 
-    private void findEvenNumbersInList(List<Integer> integerList){
+    private void findEvenNumbersInList(List<Integer> integerList) {
         integerList.stream().filter(integer -> integer % 2 == 0).sorted().forEach(System.out::println);
     }
-
 }
